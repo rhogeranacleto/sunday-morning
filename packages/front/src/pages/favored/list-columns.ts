852aae0d72331c88f78bcf9c74a85a94dc041b21
Rhoger Anacleto <rhogeranacleto@gmail.com>
2021-04-11 13:01:57 -0300

    feat(favored): disable list sorter

--- packages/front/src/pages/favored/list-columns.ts
+++ packages/front/src/pages/favored/list-columns.ts
@@ -1,15 +1,47 @@
 import { GridColumns } from '@material-ui/data-grid';
 
 export const COLUMNS: GridColumns = [
-  { field: 'name', headerName: 'Favorecido', flex: 1 },
-  { field: 'cpf_cnpj', headerName: 'CPF / CNPJ', flex: 1 },
+  {
+    field: 'name',
+    headerName: 'Favorecido',
+    flex: 1,
+    sortable: false,
+    disableClickEventBubbling: true,
+  },
+  {
+    field: 'cpf_cnpj',
+    headerName: 'CPF / CNPJ',
+    flex: 1,
+    sortable: false,
+    disableClickEventBubbling: true,
+  },
   {
     field: 'bankImage',
     headerName: 'Banco',
     flex: 1,
+    sortable: false,
+    disableClickEventBubbling: true,
     valueGetter: (params) => (params.getValue('bank') as { code: string }).code,
   },
-  { field: 'agency', headerName: 'Agência', flex: 1 },
-  { field: 'bankAccount', headerName: 'CC', flex: 1 },
-  { field: 'draft', headerName: 'Status do favorecido', flex: 1 },
+  {
+    field: 'agency',
+    headerName: 'Agência',
+    flex: 1,
+    sortable: false,
+    disableClickEventBubbling: true,
+  },
+  {
+    field: 'bankAccount',
+    headerName: 'CC',
+    flex: 1,
+    sortable: false,
+    disableClickEventBubbling: true,
+  },
+  {
+    field: 'draft',
+    headerName: 'Status do favorecido',
+    flex: 1,
+    sortable: false,
+    disableClickEventBubbling: true,
+  },
 ];
