abe522b769a7ee2e6624dab3659d6a915e23e520
Rhoger Anacleto <rhogeranacleto@gmail.com>
2021-04-11 12:02:11 -0300

    test(bank): create GET /bank/account-types

--- packages/server/test/e2e/bank.e2e-spec.ts
+++ packages/server/test/e2e/bank.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
+import { BankAccountType } from '../../src/modules/bank/bank-account-type.enum';
 import { BankModule } from '../../src/modules/bank/bank.module';
 import { seed } from '../../src/seed';
 import { TypeOrmModuleTest } from '../helpers/database';
@@ -26,6 +27,14 @@ describe('BankController (e2e)', () => {
       .expect(200);
 
     expect(body).toHaveLength(4);
+  });
+
+  it('GET /bank/account-types', async () => {
+    const { body } = await request(app.getHttpServer())
+      .get('/bank/account-types')
+      .expect(200);
+
+    expect(body).toEqual(Object.values(BankAccountType));
   });
 
   afterAll(async () => {
