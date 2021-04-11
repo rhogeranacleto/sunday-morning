9461d63058f7c8a12152d2b81811d23c237e9ef3
Rhoger Anacleto <rhogeranacleto@gmail.com>
2021-04-11 12:02:04 -0300

    feat(bank): create GET /bank/account-types

--- packages/server/src/modules/bank/bank.controller.ts
+++ packages/server/src/modules/bank/bank.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Bank } from './banck.entity';
+import { BankAccountType } from './bank-account-type.enum';
 
 @Controller('bank')
 export class BankController {
@@ -12,5 +13,10 @@ export class BankController {
   @Get()
   public getAll(): Promise<Bank[]> {
     return this.bankRepository.find();
+  }
+
+  @Get('account-type')
+  public getAccountType(): string[] {
+    return Object.values(BankAccountType);
   }
 }
