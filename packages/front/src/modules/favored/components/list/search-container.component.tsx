cc92bb922cc07b67932e71fadca3d013c24daaae
Rhoger Anacleto <rhogeranacleto@gmail.com>
2021-04-14 18:55:32 -0300

    feat(search): add delay at typing

--- packages/front/src/modules/favored/components/list/search-container.component.tsx
+++ packages/front/src/modules/favored/components/list/search-container.component.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
 } from '@material-ui/core';
 import { AddCircle, SearchOutlined } from '@material-ui/icons';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 interface IFavoredListSearchContainerProps {
@@ -19,35 +20,45 @@ interface IFavoredListSearchContainerProps {
 export const FavoredListSearchContainer = ({
   search,
   setSearch,
-}: IFavoredListSearchContainerProps) => (
-  <Box padding={3}>
-    <Grid container justify="space-between" alignItems="center">
-      <Grid item>
-        <Grid container alignItems="center">
-          <Typography variant="h6">Seus favorecidos</Typography>
-          <Link to="/new" component={IconButton} color="primary">
-            <AddCircle fontSize="large" />
-          </Link>
+}: IFavoredListSearchContainerProps) => {
+  const [searchTerm, setSearchTerm] = useState(search);
+
+  useEffect(() => {
+    const delayFn = setTimeout(() => setSearch(searchTerm), 500);
+
+    return () => clearTimeout(delayFn);
+  }, [searchTerm]);
+
+  return (
+    <Box padding={3}>
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item>
+          <Grid container alignItems="center">
+            <Typography variant="h6">Seus favorecidos</Typography>
+            <Link to="/new" component={IconButton} color="primary">
+              <AddCircle fontSize="large" />
+            </Link>
+          </Grid>
         </Grid>
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="favored-search">
+            Nome, CPF, agência ou conta
+          </InputLabel>
+          <OutlinedInput
+            id="favored-search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton aria-label="toggle password visibility" edge="end">
+                  <SearchOutlined />
+                </IconButton>
+              </InputAdornment>
+            }
+            labelWidth={210}
+          />
+        </FormControl>
       </Grid>
-      <FormControl variant="outlined">
-        <InputLabel htmlFor="favored-search">
-          Nome, CPF, agência ou conta
-        </InputLabel>
-        <OutlinedInput
-          id="favored-search"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton aria-label="toggle password visibility" edge="end">
-                <SearchOutlined />
-              </IconButton>
-            </InputAdornment>
-          }
-          labelWidth={210}
-        />
-      </FormControl>
-    </Grid>
-  </Box>
-);
+    </Box>
+  );
+};
